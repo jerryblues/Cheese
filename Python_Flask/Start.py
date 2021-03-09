d98f99f84fb39f2e680e37e8ac7a7c1a24f36ed3
h4zhang <hao.6.zhang@nokia-sbell.com>
2021-03-09 13:48:53 +0800

    update start script

--- Python_Flask/Start.py
+++ Python_Flask/Start.py
@@ -1,17 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import pytz
 import os
 
 
-# only run in linux
-
-
 def job():
-    print("====== current time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "======")
+    print("=== current time:", datetime.now(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S"), "===")
     os.system("sudo kill -9 `ps -ef|grep FB_effort.py |grep -v grep|awk '{print$2}'`")
     os.system('python3 FB_effort.py')
 
 
 scheduler = BlockingScheduler()
-scheduler.add_job(job, 'cron', max_instances=65535, day_of_week='1-5', hour='8-20', minute='0', timezone="Asia/Shanghai")
+scheduler.add_job(job, 'cron', max_instances=65535, day_of_week='mon,tue,wed,thu,fri', hour='8-20', minute='0, 10, 20, 30, 40, 50',
+                  timezone="Asia/Shanghai", next_run_time=datetime.now())
 scheduler.start()
